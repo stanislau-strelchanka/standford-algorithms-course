f53c955cfda1d6a2f4d463540098a3234ce66126
stashewski <ssvssxo@gmail.com>
2018-12-09 12:16:47 +0300

    implement recursive scc, working on small graphs

--- src/main/java/org/learn/coursera/outline/week5/graph/scc/DirectedScc.java
+++ src/main/java/org/learn/coursera/outline/week5/graph/scc/DirectedScc.java
@@ -1,15 +1,58 @@
 package org.learn.coursera.outline.week5.graph.scc;
 
 import org.learn.coursera.datastructures.graph.Graph;
-import sun.security.provider.certpath.Vertex;
+import org.learn.coursera.datastructures.graph.impl.ReversedGraph;
+import org.learn.coursera.outline.week5.graph.TopologicalSort;
+import org.learn.coursera.datastructures.graph.impl.Vertex;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class DirectedScc {
 
-    Map<Integer, List<Vertex>> find(final Graph graph) {
-        return new HashMap<>();
+    private final TopologicalSort topoSort = new TopologicalSort();
+    private final Set<Vertex> explored = new HashSet<>();
+
+    public Map<Integer, List<Vertex>> find(final Graph graph) {
+        final Map<Integer, List<Vertex>> result = new HashMap<>();
+        final List<Vertex> properlyOrderedVertices = getProperlyOrderedVertices(graph);
+        int currentScc = 1;
+        for (final Vertex vertex : properlyOrderedVertices) {
+            if (!isExplored(vertex)) {
+                final List<Vertex> vertices = new ArrayList<>();
+                vertices.add(vertex);
+
+                final List<Vertex> explored = explore(graph, vertex, vertices);
+                result.put(currentScc++, explored);
+            }
+        }
+
+        return result;
+    }
+
+    private List<Vertex> explore(final Graph graph, final Vertex vertex, final List<Vertex> vertices) {
+        explored.add(vertex);
+        for (Vertex outgoingVertex : graph.getOutgoingVertices(vertex)) {
+            if (!isExplored(outgoingVertex)) {
+                vertices.add(outgoingVertex);
+                return explore(graph, outgoingVertex, vertices);
+            }
+        }
+
+        return vertices;
+    }
+
+    private boolean isExplored(final Vertex vertex) {
+        return explored.contains(vertex);
+    }
+
+    private List<Vertex> getProperlyOrderedVertices(final Graph graph) {
+        final Graph reversedGraph = new ReversedGraph(graph);
+        //todo check why starting index is not 0;
+        final Map<Vertex, Integer> sortedVertices = topoSort.sort(reversedGraph);
+        return sortedVertices.entrySet().stream()
+                .sorted(Comparator.comparingInt(Map.Entry::getValue))
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/org/learn/coursera/outline/week5/graph/scc/DirectedSccTest.java
+++ src/test/java/org/learn/coursera/outline/week5/graph/scc/DirectedSccTest.java
@@ -2,13 +2,13 @@ package org.learn.coursera.outline.week5.graph.scc;
 
 import org.junit.jupiter.api.Test;
 import org.learn.coursera.datastructures.graph.Graph;
+import org.learn.coursera.datastructures.graph.impl.Vertex;
 import org.learn.coursera.outline.TestUtils;
-import sun.security.provider.certpath.Vertex;
 
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DirectedSccTest {
 
@@ -16,9 +16,19 @@ public class DirectedSccTest {
 
     @Test
     public void test() {
+        final Graph graph = TestUtils.readGraph("week5/graph/scc/test.txt");
+        final Map<Integer, List<Vertex>> result = directedScc.find(graph);
+
+        assertFalse(result.isEmpty());
+        assertEquals(4, result.keySet().size());
+    }
+
+    @Test
+    public void testAssignment() {
         final Graph graph = TestUtils.readCompressedGraph("week5/graph/scc/scc_asignment.gz");
         final Map<Integer, List<Vertex>> result = directedScc.find(graph);
 
         assertFalse(result.isEmpty(), "There should SCCs in a graph");
+        System.out.println(result.keySet().size());
     }
 }
