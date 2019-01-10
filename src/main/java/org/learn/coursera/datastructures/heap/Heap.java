038e9790e00b7fa0ee4bf7c7178aad37cfcefffa
stashewski <ssvssxo@gmail.com>
2019-01-10 17:33:31 +0300

    create heap datastructure interface and adapter implementation

---
+++ src/main/java/org/learn/coursera/datastructures/heap/Heap.java
@@ -0,0 +1,21 @@
+package org.learn.coursera.datastructures.heap;
+
+import java.util.List;
+
+public interface Heap {
+
+    // log(n)
+    void insert(final Integer x);
+
+    // log(n)
+    Integer extract();
+
+    // 1
+    Integer peek();
+
+    // log(n)
+    void heapify(final List<Integer> numbers);
+
+    // log(n)
+    void delete(final Integer x);
+}
---
+++ src/main/java/org/learn/coursera/datastructures/heap/PriorityQueueHeap.java
@@ -0,0 +1,48 @@
+package org.learn.coursera.datastructures.heap;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+//todo implement own heap
+public class PriorityQueueHeap implements Heap {
+
+    private final PriorityQueue<Integer> priorityQueue;
+
+    private PriorityQueueHeap(PriorityQueue<Integer> priorityQueue) {
+        this.priorityQueue = priorityQueue;
+    }
+
+    public static Heap minHeapInstance() {
+        return new PriorityQueueHeap(new PriorityQueue<>());
+    }
+
+    public static Heap maxHeapInstance() {
+        return new PriorityQueueHeap(new PriorityQueue<>(Comparator.reverseOrder()));
+    }
+
+    @Override
+    public void insert(Integer x) {
+        priorityQueue.add(x);
+    }
+
+    @Override
+    public Integer extract() {
+        return priorityQueue.poll();
+    }
+
+    @Override
+    public Integer peek() {
+        return priorityQueue.peek();
+    }
+
+    @Override
+    public void heapify(List<Integer> numbers) {
+        priorityQueue.addAll(numbers);
+    }
+
+    @Override
+    public void delete(Integer x) {
+        priorityQueue.remove(x);
+    }
+}
